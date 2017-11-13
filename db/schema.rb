bb63d90ec59ab222e94235673dcfbbf26674ca3c
NEKOTENI <riverfield.tk@gmail.com>
2017-11-13 17:14:18 +0900

    Create posts controller and post model

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 20171113080824) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
